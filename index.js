3f9fd845c49d2cccc4afe32fa973c4a7e02952a5
chaitanyaekre <chaitanya.ekre@outlook.com>
2023-06-13 18:03:22 +0530

    second day of jscript coding.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/*
+
 // Variable in javascript
 let myName ='chaitanya';
 console.log(myName);
@@ -45,3 +47,72 @@ b = c;
 console.log(a);
 console.log(b);
 
+
+
+// For in loop in javascript ( mostly used to iterate over properties in an object)
+
+let employee = {
+    name: 'chaitanya',
+    designation: 'release lead',
+    age:37
+}
+
+for(let key in employee){
+    console.log(key + " is "+ employee[key])
+}
+
+// for of loop in javascript (mostly used to iterate over arrays)
+
+const colors = ['red','green','blue'];
+
+for(let color of colors){
+    console.log(color);
+}
+
+
+
+// program to find max of two numbers
+
+function max(number1, number2){
+ let max;
+ if(number1 > number2)
+ {
+    return number1;
+ }   
+ if(number2 > number1)
+ {
+    return number2;
+ }
+ else
+ {
+    return 'equal numbers.'
+ }
+
+}
+
+let num1 = 2;
+let num2 = 5;
+
+console.log("Result is = "+ max(num1,num2));
+*/
+
+let input = 7;
+const output = fizzbuzz(input);
+console.log(output);
+
+function fizzbuzz(input){
+    if(typeof(input) !== 'number')
+        return NaN;
+    else if( isDivisibale(input,3) && isDivisibale(input,5))
+        return 'fizzbuzz'
+    else if(isDivisibale(input,3))
+        return 'fizz';
+    else if (isDivisibale(input,5))
+        return 'buzz';
+    else
+        return input;
+}
+
+function isDivisibale(divident, divisor){
+    return (divident%divisor) ==    = 0
+}
